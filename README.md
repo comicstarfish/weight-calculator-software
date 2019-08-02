dacf4a65408aae4e8f584c790190273d7162cc60
Manoj Jahgirdar <manojjahgirdar@gmail.com>
2019-08-03 00:55:02 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+## Weight Calculator Software
+### 1. Clone this git repo.
+
+### 2. Go to CMD and type run the following:
+```
+C:\Users\ganesh>pip3 install flask
+...
+
+C:\Users\manoj>pip3 install wtforms
+...
+```
+### 3. Now cd into the cloned directory:
+```
+C:\Users\ganesh>cd .\Downloads\weight-calculator-software\
+```
+### 4. Run the python script
+```
+C:\Users\ganesh\Downloads\weight-calculator-software> python3 app.py
+```
+NOTE: If this fails try `python app.py`
+### 5. At this point your CMD should display something like this:
+```
+* Serving Flask app "app" (lazy loading)
+* Environment: production   WARNING: This is a development server. Do not use it in a production deployment. Use a production WSGI server instead. 
+* Debug mode: on * Restarting with stat
+* Debugger is active!
+* Debugger PIN: 557-577-282
+* Running on http://127.0.0.1:5000/ (Press CTRL+C to quit)
+...
+```
+### 6. Hold Ctrl and click the displayed link to access the python web app.
+
---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template, request
+from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
+
+DEBUG = True
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+class SurveyForm(Form):
+    plain = TextField('Enter plain GSM:', validators=[validators.DataRequired()])
+    flute = TextField('Enter flute GSM:', validators=[validators.DataRequired()])
+    length = TextField('Enter length in inches:', validators=[validators.DataRequired()])
+    width = TextField('Enter width in inches:', validators=[validators.DataRequired()])
+    sheet_count = TextField('Enter sheet count:', validators=[validators.DataRequired()])
+
+@app.route('/', methods=['GET', 'POST'])
+def hello():
+    form = SurveyForm(request.form)
+    print(form.errors)
+
+    if request.method == 'POST':
+        plain = float(request.form['plain'])
+        flute = float(request.form['flute'])
+        length = float(request.form['length'])
+        width = float(request.form['width'])
+        sheet_count = float(request.form['sheet_count'])
+
+        sheet_area = (length*width*25.4*25.4)/(1000*1000)
+        total_GSM = plain+(1.5*flute)
+        sheet_weight = total_GSM * sheet_area
+        production_weight = sheet_weight * sheet_count
+
+        if production_weight>1000:
+            production_weight1 = production_weight/1000
+            return render_template('index.html', form=form, flute_weight=flute, plain_weight=plain, sheet_area=sheet_area, total_GSM=total_GSM, sheet_count=sheet_count, sheet_weight=sheet_weight, production_weight=str(production_weight1)+" kg")
+        else:
+            return render_template('index.html', form=form, flute_weight=flute, plain_weight=plain, sheet_area=sheet_area, total_GSM=total_GSM, sheet_count=sheet_count, sheet_weight=sheet_weight, production_weight=str(production_weight)+" grams")
+   
+    return render_template('index.html', form=form)
+
+if __name__ == '__main__':
+    app.run()
